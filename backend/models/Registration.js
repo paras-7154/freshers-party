c9b2c9b3ca374e1a83fbc00669261b8a50715a1d
paras-7154 <parsu7154@gmail.com>
2026-08-30 22:23:18 +0530

    Show song name for select1e

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -1,11 +1,32 @@
 const mongoose = require("mongoose");
 
 const registrationSchema = new mongoose.Schema({
-  studentName: { type: String, required: true },
-  className: { type: String, required: true },
-  mobile: { type: String, required: true },
-  event: { type: String, required: true },
-  songName: { type: String, required: true }
+  studentName: {
+    type: String,
+    required: true
+  },
+
+  className: {
+    type: String,
+    required: true
+  },
+
+  mobile: {
+    type: String,
+    required: true
+  },
+
+  event: {
+    type: String,
+    required: true
+  },
+
+  songName: {
+    type: String,
+    required: false,
+    default: ""
+  }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Registration", registrationSchema);
